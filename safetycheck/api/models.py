4211a87f52da3555aa418933d7ae0e224a9b9b7a
rattanashish <33751310+rattanashish@users.noreply.github.com>
2018-01-14 20:58:35 +0530

    Update models.py

--- safetycheck/api/models.py
+++ safetycheck/api/models.py
@@ -7,7 +7,7 @@ class user_bac_video(models.Model):
     bac_level = models.FloatField(null=True, blank=True)
     user_video = models.FileField(upload_to='user_videos/')
     user = models.ForeignKey(User,related_name='user_bac', on_delete=models.CASCADE,)
-)
+
     timestamp = models.TimeField(auto_now_add=True)
 
 
