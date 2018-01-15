b3d0717ab5c3ede7508d0a6d29dd83c265b3a8a1
rattanashish <33751310+rattanashish@users.noreply.github.com>
2018-01-16 00:56:39 +0530

    Update settings.py

--- safetycheck/safetycheck/settings.py
+++ safetycheck/safetycheck/settings.py
@@ -130,6 +130,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.10/howto/static-files/
 
 STATIC_URL = '/static/'
+PROJECT_DIR = os.path.dirname(os.path.abspath(__file__))
+STATIC_ROOT = os.path.join(PROJECT_DIR, 'static')
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
 
