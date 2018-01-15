331f708a732f65c46984525b0848e22842a08e93
rattanashish <33751310+rattanashish@users.noreply.github.com>
2018-01-16 00:51:21 +0530

    Update settings.py

--- safetycheck/safetycheck/settings.py
+++ safetycheck/safetycheck/settings.py
@@ -84,11 +84,14 @@ WSGI_APPLICATION = 'safetycheck.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'urban_prod',
+        'USER': 'u_urban',
+        'PASSWORD': '123',
+        'HOST': 'localhost',
+        'PORT': '',
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/1.10/ref/settings/#auth-password-validators
